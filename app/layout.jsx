001835c3aa516e2312438c9badb0a7a939208163
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2025-12-12 08:45:43 +0000

    layout.jsx: layout html added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,17 @@
-// 
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@components/Footer";
+
+export default function RootLayout({ children }) {
+    return(
+        <html lang="eng">
+            <body>
+                <Header/>
+                <main style={{ padding: 20 }}>
+                    {children}
+                </main>
+                <Footer/>
+            </body>
+        </html>
+    );
+}
