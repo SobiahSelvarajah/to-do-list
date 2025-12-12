a9634cc18af133b0cbf3f9f32cbd1a615380d42f
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2025-12-12 08:49:40 +0000

    page.jsx: page initial code added

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,10 @@
-// 
+
+export default function Home() {
+    return (
+        <>
+            <h1 className="page__heading">
+                Homepage me hearty!
+            </h1>
+        </>
+    );
+} 
